678762302e5a96c02c64905837abb1e4b0f5fb63
mariopino <info@quequiereshacer.es>
2019-09-27 11:13:28 +0200

    Subscribe to new blocks and get validator set

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-let { RpcClient } = require('tendermint')
+const { RpcClient } = require('tendermint')
+const client = RpcClient('wss://lunie.mariopino.es:46657')
 
-// some full node's RPC port
-//let client = RpcClient('wss://lunie.mariopino.es:46657')
+// Subscribe to new blocks and get validator set
+async function subscribeToNewBlock() {
+  client.subscribe({ query: "tm.event = 'NewBlock'"}, async (res, err)=> {
+    let height = res.block.header.height
+    console.log(`Block: #${height}`)
+    console.log(res)
+    const validators = await getValidators(height)
+    console.log(`Validators set for block: #${height}`)
+    console.log(validators)
+    const tx = await getTx(height)
+    console.log(tx)
+  })
+}
 
-// request a block
-//client.block({ height: 100 })
-//  .then((res) => console.log(res))
+/* DIDN'T WORK */
+async function subscribeToNewTx() {
+  client.subscribe({ query: "tx.height = 10"}, (event) => {
+    console.log(event)
+  })
+}
 
-let client = RpcClient('wss://lunie.mariopino.es:46657')
+async function getBlock(blockHeight) {
+  client.block({ height: blockHeight})
+    .then((res) => console.log(res))
+}
 
-client.subscribe({ query: "tm.event = 'NewBlock'"}, (event, err) => {
-  console.log(event)
-  /*
-    {
-      name: 'NewBlock',
-      data: {
-        type: 'new_block',
-        data: {
-          block: [
-            {
-              txs: []
-            }
-          ]
-        }
-      }
-    }
-  */
-})
+async function getValidators(blockHeight) {
+  return validators = await client.validators({ height: blockHeight})
+}
+
+async function getTx(blockHeight) {
+  return tx = await client.tx({ query: `tx.height=${blockHeight}`})
+}
+
+
+//subscribeToNewBlock()
+subscribeToNewTx()
+
+// for(let height = 1; height <= 100; height++) {
+//  getBlock(1);
+// }
